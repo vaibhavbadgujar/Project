c0caba3aa5f89b024019dedbf68ca1323ff2320d
vaibhavbadgujar <91780179+vaibhavbadgujar@users.noreply.github.com>
2023-06-05 22:48:16 +0530

    update

--- login page/index.js
+++ login page/index.js
@@ -7,12 +7,18 @@ const iconClose = document.querySelector('.icon-close');
 registerLink.addEventListener('click', ()=>{
     wrapper.classList.add('active');
 })
+
 loginLink.addEventListener('click', ()=>{
     wrapper.classList.remove('active');
+
 })
 btnPopup.addEventListener('click', ()=>{
     wrapper.classList.add('active-popup');
+
 })
+
 iconClose.addEventListener('click', ()=>{
     wrapper.classList.remove('active-popup');
+    
+
 })
