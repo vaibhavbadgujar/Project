0dbdc544d38665eb34eca589b3a17fa6dda334ad
vaibhavbadgujar <91780179+vaibhavbadgujar@users.noreply.github.com>
2023-08-21 01:15:49 +0530

    .

--- responsive-car-website-main/assets/js/main.js
+++ responsive-car-website-main/assets/js/main.js
@@ -44,6 +44,25 @@ const scrollHeader = () =>{
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== POPULAR SWIPER ===============*/
+var swiperPopular = new Swiper(".popular__container", {
+    loop: true,
+    spaceBetween: 24,
+    sliderPerView: 'auto',
+    grabCursor: true,
+
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    breakpoints: {
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          spaceBetween: 48,
+        },
+      },
+  });
 
 
 /*=============== MIXITUP FILTER FEATURED ===============*/
