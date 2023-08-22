a14b796603eaa59839ebcc3738453a731da8f709
vaibhavbadgujar <91780179+vaibhavbadgujar@users.noreply.github.com>
2023-08-22 13:43:57 +0530

    .

--- responsive-car-website-main/assets/js/main.js
+++ responsive-car-website-main/assets/js/main.js
@@ -4,19 +4,16 @@ const navMenu = document.getElementById('nav-menu'),
       navClose = document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
-/* Validate if constant exists */
 if (navToggle) {
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu');
-    });
+  navToggle.addEventListener('click', () => {
+      navMenu.classList.add('show-menu');
+  });
 }
 
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
 if (navClose) {
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu');
-    });
+  navClose.addEventListener('click', () => {
+      navMenu.classList.remove('show-menu');
+  });
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
