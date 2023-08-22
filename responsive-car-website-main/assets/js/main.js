9e54052972f8001b5d3ef68b717fc56c2fe2f39c
vaibhavbadgujar <91780179+vaibhavbadgujar@users.noreply.github.com>
2023-08-22 12:03:39 +0530

    .

--- responsive-car-website-main/assets/js/main.js
+++ responsive-car-website-main/assets/js/main.js
@@ -66,15 +66,54 @@ var swiperPopular = new Swiper(".popular__container", {
 
 
 /*=============== MIXITUP FILTER FEATURED ===============*/
+let mixerFeatured = mixitup('.featured__content', {
+  selectors: {
+      target: '.featured__card'
+  },
+  animation: {
+      duration: 300
+  }
+});
 
 
 /* Link active featured */ 
+const linkFeatured = document.querySelectorAll('.featured__item')
+
+function activeFeatured(){
+  linkFeatured.forEach(l=> l.classList.remove('active-featured'))
+  this.classList.add('active-featured')
+}
+linkFeatured.forEach(l=>l.addEventListener('click', activeFeatured))
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
