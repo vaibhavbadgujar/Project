e6d223fc9ca04fabe3603d078fb2d27c6b1a07a0
vaibhavbadgujar <91780179+vaibhavbadgujar@users.noreply.github.com>
2023-08-28 13:37:43 +0530

    Responsive Travel Website

--- Responsive-Travel-Website/assets/js/main.js
+++ Responsive-Travel-Website/assets/js/main.js
@@ -43,9 +43,50 @@ window.addEventListener('scroll', blurHeader)
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({   
+    origin: 'top',
+    distance: '60px',
+    duration: 3000,
+    delay: 400,
+    //reset: true // animation repeat
+
+})
+
+sr.reveal('.home__data , explore__data, explore__user, .footer__container')
+sr.reveal('.home__card', {delay: 600 , distance: '100px', interval: 100})
+sr.reveal('.about__data , .join__image', {origin: 'right'})
+sr.reveal('.about__image, .join__data', {origin: 'left'})
+sr.reveal('.popular__card', { interval: 200})
+
